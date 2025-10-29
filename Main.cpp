8fa95bee8823bb00dce06da02b40df2c09117b9f
craftair <91932367+craftair@users.noreply.github.com>
2025-10-30 05:15:40 +0900

    ミス修正

--- Main.cpp
+++ Main.cpp
@@ -44,7 +44,7 @@ void Settings(GameData& data, App& manager, Array<Texture> textures)
 
 void Main()
 {
-	Window::Resize(1920, 1080);
+	Window::Resize(1280, 720);
 
 	FontAsset::Register(U"MisakiFont", FontMethod::MSDF, 16, U"resources/font/misaki/misaki_gothic.ttf");
 
