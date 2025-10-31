d0af030d78a6d65f86ff0808f44f2bcdeac7a07e
Pictogram23 <shanchengyinger@gmail.com>
2025-11-01 04:33:43 +0900

    モノローグを本格実装

--- Monolog.cpp
+++ Monolog.cpp
@@ -16,13 +16,16 @@ Monolog::Monolog(const InitData& init)
 		message.text = object[U"text"].getString();
 		m_messages.push_back(message);
 	}
+
+	stopwatch.start();
 }
 
 void Monolog::update()
 {
-	if (m_serifWindow.leftClicked())
+	if (isFinished() && m_serifWindow.leftClicked())
 	{
 		message_index++;
+		stopwatch.restart();
 		if (message_index >= m_messages.size())
 		{
 			changeScene(State::Title);
@@ -34,15 +37,34 @@ void Monolog::draw() const
 {
 	Scene::SetBackground(ColorF{ 1.0 });
 
+	m_cgArea(texture2(0, 0, 1920, 1080)).draw();
 	m_serifWindow(texture1(0, 0, 837, 186)).draw();
 
 	const Font& font = FontAsset(U"MisakiFont");
+
+	const int32 count = Max(((stopwatch.ms() - 200) / 24), 0);
+
 	if (message_index < m_messages.size())
 	{
-		font(U"{}"_fmt(m_messages[message_index].text)).draw(32, m_serifWindow.tl() + Vec2{ 32, 32 }, ColorF{ 0.0 });
+		font(U"{}"_fmt(m_messages[message_index].text.substr(0, count))).draw(32, m_serifWindow.tl() + Vec2{ 32, 32 }, ColorF{ 0.0 });
 	}
 	else
 	{
-		font(U"{}"_fmt(m_messages[m_messages.size() - 1].text)).draw(32, m_serifWindow.tl() + Vec2{32, 32}, ColorF{0.0});
+		font(U"{}"_fmt(m_messages[m_messages.size() - 1].text)).draw(32, m_serifWindow.tl() + Vec2{ 32, 32 }, ColorF{ 0.0 });
 	}
+
+	if (isFinished())
+	{
+		Triangle{ m_serifWindow.br().movedBy(-48, -42), 20, 180_deg }.draw(ColorF{ 0.0, Periodic::Sine0_1(2.0s) });
+	}
+}
+
+bool Monolog::isFinished() const
+{
+	if (message_index >= m_messages.size())
+	{
+		return true;
+	}
+	const int32 count = Max(((stopwatch.ms() - 200) / 24), 0);
+	return (static_cast<int32>(m_messages[message_index].text.length()) <= count);
 }
