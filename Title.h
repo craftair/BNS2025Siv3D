0140fe0b01279fc82898c3a75837269fd3878e3f
Pictogram23 <shanchengyinger@gmail.com>
2025-11-01 12:38:15 +0900

    タイトル画面の実装完了

--- Title.h
+++ Title.h
@@ -7,5 +7,14 @@ public:
 	Title(const InitData& init);
 	void update() override;
 	void draw() const override;
-};
 
+private:
+	const Texture texture1{ U"resources/texture/title-cg.png" };
+	const Texture texture2{ U"resources/texture/start-btn.png" };
+	const Texture texture3{ U"resources/texture/start-btn-selected.png" };
+	const Texture texture4{ U"resources/texture/option-btn.png" };
+	const Texture texture5{ U"resources/texture/option-btn-selected.png" };
+	RectF m_titleCg{ 0, 0, 1280, 720 };
+	RectF m_startBtn{ 462, 393, 356, 88 };
+	RectF m_optionBtn{ 436, 513, 408, 88 };
+};
